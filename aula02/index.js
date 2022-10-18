d14af5831b8066f7dd1609221d85205896edb18d
Adriana sm <a.s.m27267@gmail.com>
2022-10-18 20:55:47 -0300

    implementando rotas paralistar varios livros, listar apenas um livro , e criar um livro

--- aula02/index.js
+++ aula02/index.js
@@ -1,16 +1,26 @@
 const express = require("express");
 
 const app = express();
+const bodyParser = require("body-parser");
 
 const hostname = "192.168.3.85";
 const port = 5000;
 
-const books=[
-    {_id:1, title: 'O Dia do Curinga', autor:'Jostein Gaarden', favorito:true},
-    {_id:2, title: 'O mundo de Sophia', autor:'Jostein Gaarden', favorito:true},
-    {_id:1, title: 'A casa', autor:'Raquel de Queiroz', favorito:false}, 
-    
-]
+const books = [
+  {
+    _id: 1,
+    title: "O Dia do Curinga",
+    autor: "Jostein Gaarden",
+    favorito: true,
+  },
+  {
+    _id: 2,
+    title: "O mundo de Sophia",
+    autor: "Jostein Gaarden",
+    favorito: true,
+  },
+  { _id: 3, title: "A casa", autor: "Raquel de Queiroz", favorito: true },
+];
 
 app.use(
   express.urlencoded({
@@ -18,17 +28,30 @@ app.use(
   })
 );
 
+app.use(express.json());
+
 //uri
 app.get("/", (req, res) => {
   res.json({ nome: "Adriana S!" });
 });
 
 app.get("/html", (req, res) => {
+  res.sendFile(`${__dirname}/index.html`);
+});
 
-  res.sendFile(`${__dirname}/index.html`)
-})
-app.get('/books', (req,res)=>{
-    res.send(books.filter(i=> !i.favorito))
-})
+app.get("/books", (req, res) => {
+  res.send(books.filter((i) => !i.favorito));
+});
+
+app.post("/books", (req, res) => {
+  books.push(req.body);
+  res.send(req.body);
+});
+
+app.get("/books/:id", (req, res) => {
+  const book = books.find((book) => book._id == req.params.id);
+  if (book) res.send(book);
+  else res.sendStatus(404);
+});
 
 app.listen(port, hostname, () => console.log(`http://${hostname}:${port}`));
